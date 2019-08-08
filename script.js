7ea048b146f0e8f582d1a6458811cd61dd2264de
Lerita <valeriam@gmail.com>
2019-08-08 10:42:06 -0300

    ready

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const grid = document.getElementById('grid');
 const thumbnails = document.getElementById('thumbnails');
-const loadMore = document.getElementById('load-more');
-let currentPage = 1;
+const nextPage = document.getElementById('next_page');
 
 const username = document.getElementById('username');
 const name = document.getElementById('name');
@@ -18,44 +17,56 @@ const getAllProfiles = (url) => {
     fetch(url)
         .then(response => response.json())
         .then(data => {
-                const gridModel = grid.children[0];
-                const profiles = data.results;
-                grid.innerHTML = '';
+            const gridModel = grid.children[0];
+            const profiles = data.results;
+            grid.innerHTML = '';
 
-                for (const profile of profiles) {
-                    let newProfile = gridModel.cloneNode(true);
-                    newProfile.children[0].src = profile.picture.thumbnail;
-                    newProfile.children[1].innerHTML = `${profile.name.first} ${profile.name.last}`;
-                    newProfile.children[2].innerHTML = profile.location.city;
-                    newProfile.children[3].innerHTML = profile.location.state;
-                    grid.appendChild(newProfile);
+            for (const profile of profiles) {
+                let newProfile = gridModel.cloneNode(true);
+                newProfile.children[0].src = profile.picture.thumbnail;
+                newProfile.children[1].innerHTML = `${profile.name.first} ${profile.name.last}`;
+                newProfile.children[2].innerHTML = `${profile.location.city},`;
+                newProfile.children[3].innerHTML = profile.location.state;
+                grid.appendChild(newProfile);
 
 
-                    newProfile.onclick = () => {
-                        visible.style.visibility = 'visible';
-                        stopScrolling.classList.add('stop-scrolling');
-                        // largeImage.style.backgroundImage = `url(${profile.picture.large})`;
-                        largeImage.src = profile.picture.large;
-                        username.innerText = `Username: ${profile.login.username}`;
-                        name.innerText = `Name: ${profile.name.first} ${profile.name.last}`;
-                        email.innerText = `E-mail: ${profile.email}`;
-                        age.innerText = `Age: ${profile.dob.age}`;
-                    }
+                newProfile.onclick = () => {
+                    visible.style.visibility = 'visible';
+                    stopScrolling.classList.add('stop-scrolling');
+                    largeImage.src = profile.picture.large;
+                    username.innerHTML = `<b>Username:</b> ${profile.login.username}`;
+                    name.innerHTML = `<b>Name:</b> ${profile.name.first} ${profile.name.last}`;
+                    email.innerHTML = `<b>E-mail:</b> ${profile.email}`;
+                    age.innerHTML = `<b>Age:</b> ${profile.dob.age}`;
                 }
-                
-
+            }
         })
         .catch();
 }
 
-getAllProfiles(`https://randomuser.me/api/?page=${currentPage}&results=25&seed=abc`);
+getAllProfiles(`https://randomuser.me/api/?page=1&results=24&seed=abc`);
 
 
-loadMore.onclick = () => {
-    currentPage++;
-    getAllProfiles(`https://randomuser.me/api/?page=${currentPage}&results=25&seed=abc`);
+let unselectPage = 1;
 
+const getPage = () => {
+    for (let i = 1; i < 6; i++) {
+        let currentPage = document.getElementById(`page_${i}`);
+
+        currentPage.onclick = () => {
+            const selectedPage = document.getElementById(`page_${unselectPage}`)
+            selectedPage.classList.remove("selected");
+            currentPage.classList.add("selected");
+
+            getAllProfiles(`https://randomuser.me/api/?page=${i}&results=24&seed=abc`);
+
+            unselectPage = i;
+        }
+
+    }
 }
+getPage();
+
 
 closeContainer.onclick = () => {
     visible.style.visibility = 'hidden';
